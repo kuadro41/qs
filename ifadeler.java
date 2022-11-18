a1733d0686e398d5962199ac5a0365a28ed88aee
kuadro41 <73777268+kuadro41@users.noreply.github.com>
2022-11-18 15:54:36 +0300

    Create ifadeler.java

---
+++ ifadeler.java
@@ -0,0 +1,13 @@
+public class ifadeler {
+	public static void main (String[] args) {
+		int i; // expression
+		int x = 10; //decleration
+		System.out.print("aaaa");
+		double pow = Math.pow(5, 4);
+		System.out.println(pow);
+		
+		for (int j = 0; j<10 ; j++ ) {
+			System.out.println(j);
+		}
+	}
+}
