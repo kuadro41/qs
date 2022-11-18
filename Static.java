a423c59d2d96e436f3b5d46e9a3027c6b0121f31
kuadro41 <73777268+kuadro41@users.noreply.github.com>
2022-11-18 15:03:27 +0300

    Create Static.java

---
+++ Static.java
@@ -0,0 +1,16 @@
+public class Static{
+	public static void main (String [] args) {
+		System.out.println(Math.PI);
+		
+		System.out.println(Math.pow(5, 3));
+		
+		System.out.println(Math.sqrt(125));
+		
+		
+		float f  = 1.15f;
+		System.out.println(Math.ceil(f));
+		System.out.println(Math.floor(f));
+		System.out.println(Math.round(f));
+		System.out.println(Math.random()*88);
+		}
+}
