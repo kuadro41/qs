44a48d4ee287084de5df7ab4d71e52cefa018a37
kuadro41 <73777268+kuadro41@users.noreply.github.com>
2022-11-18 16:57:13 +0300

    Create aritmatiksel.java

---
+++ aritmatiksel.java
@@ -0,0 +1,21 @@
+public class aritmatiksel {
+	public static void main (String []args) {
+		int x = 20;
+		int y = 30;
+		int z = (y + x );
+		System.out.println(y+x+z); //toplama
+		System.out.println(z-y); //çıkarma
+		System.out.println(z*y); //çarpma
+		System.out.println(z/x); //bölüm
+		
+		System.out.println(5%2); // kalan
+		int  a = 20;
+		a+=20;
+		a = a+20;
+		System.out.println(a);
+		
+		int q = 33;
+		System.out.println(q++);
+		System.out.println(q);
+	}
+}
