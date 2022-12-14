db433a738c8d800fea3f9b712f6622822a31e630
kuadro41 <73777268+kuadro41@users.noreply.github.com>
2022-12-14 18:53:19 +0300

    Create ılıskısel_operator.java

---
+++ ılıskısel_operator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ılıskısel_operator {
+	public static void main (String [] args) {
+		boolean sonuc = 2 > 5 ;
+		System.out.println(sonuc);
+		
+		
+		System.out.print("2 değer giriniz:");
+		Scanner scanner = new Scanner(System.in);
+		String  a = scanner.nextLine();
+		String  b = scanner.nextLine();
+		System.out.println(a.equals(b));	
+	}
+}
