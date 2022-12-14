b49c84ade5d4dd6cb68730216a5324e6ca84007a
kuadro41 <73777268+kuadro41@users.noreply.github.com>
2022-12-14 17:22:51 +0300

    Create ilişkisel_operatörler.java

---
+++ ilişkisel_operatörler.java
@@ -0,0 +1,22 @@
+public class ilişkisel_operatörler {
+	public static void main (String[]args) {
+		int x = 10;
+		int y = 20;
+		
+		System.out.println(x < y);  // 'x','y'den küçüktür
+		System.out.println(x > y);  // 'x','y'den büyüktür
+		System.out.println(x >= y); // 'x','y'den büyük ve eşittir
+		System.out.println(x <= y); // 'x','y'den küçük ve eşittir
+		
+		System.out.println(x == y); // 'x','y'ye eşittir
+		System.out.println(x != y); // 'x','y'ye eşit değildir
+		
+		
+		boolean b = x !=y ;         // 'x','y'ye eşit değildir 'b'ye atanır
+		System.out.println(b);      // atanan 'b' yazdırılır 
+		
+		
+	}
+	
+
+}
